f4c5e451315687b5ef5feb600f77110a40bc7175
Yusuke Kato <wtsnyk13@gmail.com>
2017-04-29 11:58:10 +0900

    /api/sampleをはやして/api/poiも用意

--- server-side/app/Http/routes.php
+++ server-side/app/Http/routes.php
@@ -14,3 +14,57 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+
+  {
+  "name": "tokyo-tower",
+  "lat": "35.658611",
+  "lng": "139.745556",
+  "explain": "this is japanese tokyo tower",
+  "images": [
+  "https://hogehoge.com/hoge.png",
+  "https://hogehoge.com/fuga.png",
+  "https://hogehoge.com/piyo.png"
+  ]
+  },
+
+ */
+Route::get('/api/sample', function () {
+    $response = array();
+
+    
+    $poi["name"] = "Tokyo Tower";
+    $poi["lat"] = 35.658611;
+    $poi["lon"] = 139.745556;
+    $poi["explain"] = "this is japanese tokyo tower";
+    $poi["images"] = array(
+        "https://hogehoge.com/hoge.png",
+        "https://hogehoge.com/fuga.png",
+        "https://hogehoge.com/piyo.png"
+      );
+
+    //$response["message"] = "No problem";
+
+    $response[] = $poi;
+
+    $poi["name"] = "Mt.Fuji";
+    $poi["lat"] = 35.360628;
+    $poi["lon"] = 138.727365;
+    $poi["explain"] = "FUJI-SAAAAAAAAN";
+    $poi["images"] = array(
+        "https://hogehoge.com/hoge.png",
+        "https://hogehoge.com/fuga.png",
+        "https://hogehoge.com/piyo.png"
+      );
+
+    $response[] = $poi;
+
+    return Response::json($response);
+
+});
+
+
+Route::get('/api/poi', function () {
+    return view('welcome');
+});
