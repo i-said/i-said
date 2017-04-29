c92b8a9c7c2be3c5ba6f850477b2946cfff77356
shinji ichien <shinji11088011@gmail.com>
2017-04-29 21:27:53 +0900

    commit

--- db/insert.php
+++ db/insert.php
@@ -35,13 +35,12 @@ while (($data = fgetcsv($temp, 0, "\t")) !== FALSE) {
   $lat = $data[0];
   $lng = $data[1];
   $image_url = $data[2];
+  $desc = $data[3];
   $name = str_replace("http://dbpedia.org/resource/", "",$data[3]);
   $name = str_replace("Třebíč", "Trebic",$data[3]);
   $name = str_replace("Třebí�?", "Trebic",$data[3]);
-  // $name = str_replace(""", "\"",$name);
 
-  // echo $i . ' ' . $lat . ' ' . $lng  . "\n";
-  $sql .= insert($name, "hoge", $lat, $lng, $image_url);
+  $sql .= insert($name, $desc, $lat, $lng, $image_url);
 }
 //ファイルを作成
 $sql_file = "sql_file.sql";
